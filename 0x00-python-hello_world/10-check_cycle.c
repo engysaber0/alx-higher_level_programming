91b0c40ebb6b7784ab0248f69c78644c1e1eaa08
engysaber0 <engy8174@gmail.com>
2023-10-30 13:11:57 -0700

    circular linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * 
+ * @list: ptr to the list
+ *
+ * Return: 0 if there is no cycle, 1 if there
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *first, *second;
+
+if (list)
+return (0);
+
+first = list;
+second = list->next;
+
+while (first && second && second->next)
+{
+first = first->next;
+second = second->next->next;
+if (first == second)
+return (1);
+}
+
+return (0);
+}
