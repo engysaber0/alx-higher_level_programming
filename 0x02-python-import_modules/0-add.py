0e337ec7f2e4696ad52d384e3cad0043b05ca1f3
engysaber0 <engy8174@gmail.com>
2023-11-02 17:19:42 -0700

    Write a program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from add_0 import add
-    a = 1
-    b = 2
-# Calculate the result using the imported function
-result = add(a, b)
+from add_0 import add
 
-# Print the result using string formatting
-print("{:d} + {:d} = {:d}".format(a, b, result))
+a = 1
+b = 2
 
+if __name__ == "__main__":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
