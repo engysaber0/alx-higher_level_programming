108590aba24f69a1e5e82117d81eafdd25a5fd6d
engysaber0 <engy8174@gmail.com>
2024-02-08 16:45:42 -0800

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Lookup object Module.
+Creates a function with the prototype lookup(obj).
+"""
+def lookup(obj):
+    """
+    Funcion that returns list of available attributes
+    and methods of an object.
+    """
+    return dir(obj)
